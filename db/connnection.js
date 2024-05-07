926b0378b8e7b9efbe68e15ffe693639741e9779
manuel perez <manuelperez.0000@gmail.com>
2024-05-07 15:06:52 -0400

    probando mongodb url nueva

--- db/connnection.js
+++ db/connnection.js
@@ -6,7 +6,25 @@ const config = require('../config.js');
 
 class CallDB {
     connectToDB() {
-        const { MONGO_PROD_URL, MONGO_DEV_URL, NODE_ENV } = process.env;
+
+        const { MONGO_PROD_URL } = process.env
+
+        const url = MONGO_PROD_URL
+
+        mongoose.set('strictQuery', false);
+        
+        mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            dbName: config.databaseName,
+        }).then(() => {
+            console.log('Connected to the database');
+        }).catch((error) => {
+            console.log(error);
+        })
+
+
+        /* const { MONGO_PROD_URL, MONGO_DEV_URL, NODE_ENV } = process.env;
 
         const url = NODE_ENV === 'production' ? MONGO_PROD_URL : MONGO_DEV_URL;
 
@@ -20,13 +38,13 @@ class CallDB {
             console.log('Connected to the database');
         }).catch((error) => {
             console.log(error);
-        })
+        }) */
     }
 
-    async disconectToDB() {
+    /* async disconectToDB() {
         await mongoose.disconnect();
         console.log('Disconnected from the database');
-    }
+    } */
 }
 
 module.exports = CallDB;
